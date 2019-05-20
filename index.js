27589601432dbf4b3e473c9e3e36e45a4011db2e
JeffLawrence1 <jefflaw13@hotmail.com>
2019-05-20 16:13:59 -0700

    refactor for array

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
 // console.log(greet('JOHN'));
-console.log(math.add(1,3)); // 4
-console.log(math.subtract(1,3)); // -2
-console.log(math.multiply(1,3)); // 3
+console.log(math.add([1,3,4])); // 8
+console.log(math.subtract([1,3,4])); // -8
+console.log(math.multiply([1,3,4])); // 12
 console.log(math.divide(4,2)); //2
