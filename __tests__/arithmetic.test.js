4fdc66167e9888691c841ef4384ec22f5135dbe3
JeffLawrence1 <jefflaw13@hotmail.com>
2019-05-20 18:22:16 -0700

    work on testing

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,4 +1,40 @@
 'use strict';
 
-const arithmetic = require('../lib/arithmetic.js');
+const faker = require('faker');
 
+const arithmetic = require('../lib/arithmetic.js');
+const greet = require('../lib/greet.js');
+
+describe('Arithmetic Module', () => {
+  let a = faker.random.number();
+  console.log(a);
+  let b = faker.random.number();
+  console.log(b);
+  let c = faker.random.number();
+  console.log(c);
+  let d = faker.random.word();
+  console.log(d);
+  let e = faker.random.word();
+  let test = [a, b, c];
+
+  it('add should not take a string', () => {
+    expect(arithmetic.add([d, e])).toBeNull();
+  });
+
+  it('add should add', () => {
+    expect(arithmetic.add(test)).toEqual(a+b+c);
+  });
+
+  it('subtract should subtract', () => {
+    expect(arithmetic.subtract(test)).toEqual((a-b-c));
+  });
+
+  it('multiply should multiply', () => {
+    expect(arithmetic.multiply(test)).toEqual((a*b*c));
+  });
+
+  it('divide should divide', () => {
+
+    expect(arithmetic.divide(a, b)).toEqual((a/b));
+  });
+});
